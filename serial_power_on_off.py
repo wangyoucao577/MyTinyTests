4339cc4432ade3d467fca66b0f99507bbf210a4f
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2016-01-07 22:53:00 +0800

    按照Google建议规范，把主功能放入main中，以实现可导入性。

--- serial_power_on_off.py
+++ serial_power_on_off.py
@@ -1,4 +1,4 @@
-#!/root/.pyenv/versions/3.5.1/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 
@@ -9,6 +9,7 @@ import traceback
 
 power_on_cmd = [0x55, 0x01, 0x12, 0x00, 0x00, 0x00, 0x01, 0x69]
 power_off_cmd = [0x55, 0x01, 0x11, 0x00, 0x00, 0x00, 0x01, 0x68]
+
 
 def scan_comports_out():
 	comport_list = list(serial.tools.list_ports.comports())
@@ -32,21 +33,24 @@ def power_operate_com(comport, cmd):
 		#traceback.print_exc()
 		
 #Working start
-available_comports = scan_comports_out()
-available_commands = ["ON", "OFF"]
-operation_commands = {}
-operation_commands[available_commands[0]] = power_on_cmd
-operation_commands[available_commands[1]] = power_off_cmd
-#添加字典元素的方法二
-#operation_commands.update({available_commands[0]:power_on_cmd})
-#operation_commands.update({available_commands[1]:power_off_cmd})
+def main():
+    available_comports = scan_comports_out()
+    available_commands = ["ON", "OFF"]
+    operation_commands = {}
+    operation_commands[available_commands[0]] = power_on_cmd
+    operation_commands[available_commands[1]] = power_off_cmd
+    #添加字典元素的方法二
+    #operation_commands.update({available_commands[0]:power_on_cmd})
+    #operation_commands.update({available_commands[1]:power_off_cmd})
 
-#print(sys.argv)
-if (len(sys.argv) < 3 or (sys.argv[1] not in available_comports) or (sys.argv[2].upper() not in available_commands)) :
-	print("Usage: ")
-	print("      python serial_power_on_off.py [COM1|COM2|...] [ON|OFF]")
-	print("Available Serial Ports: ", available_comports)
-else:
-	power_operate_com(sys.argv[1], operation_commands[sys.argv[2].upper()])
-	
-	
+    #print(sys.argv)
+    if (len(sys.argv) < 3 or (sys.argv[1] not in available_comports) or (sys.argv[2].upper() not in available_commands)) :
+        print("Usage: ")
+        print("      python serial_power_on_off.py [COM1|COM2|...] [ON|OFF]")
+        print("Available Serial Ports: ", available_comports)
+    else:
+        power_operate_com(sys.argv[1], operation_commands[sys.argv[2].upper()])
+        
+
+if __name__ == '__main__':
+    main()
