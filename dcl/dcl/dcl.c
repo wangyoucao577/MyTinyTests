558ce153b73055146fa45114df28df3fd02c6dba
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2016-01-19 00:03:56 +0800

    dcl部分代码摘抄过来。 TODO：1、gettoken和main相关代码；2、理解梳理。

--- dcl/dcl/dcl.c
+++ dcl/dcl/dcl.c
@@ -1,4 +1,78 @@
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/* dcl: parse a declarator*/
+void dcl();
+
+/* dirdcl: parse a direct declarator*/
+void dirdcl();
+
+/* return next token*/
+int gettoken();
+
+#define MAXTOKEN 100
 
 
+int tokentype;  /* type of last token*/
+char token[MAXTOKEN];   /* last token string*/
+char name[MAXTOKEN];    /* identifier name*/
+char datatype[MAXTOKEN];    /* data type = char, int, etc.*/
+char out[1000];     /* output string*/
 
+enum { 
+    NAME = 1, 
+    PARENS, 
+    BRACKETS };
+
+int main()
+{
+
+}
+
+void dcl(void)
+{
+    int ns;
+
+    for (ns = 0; gettoken() == '*'; )   //count *'s
+        ns++;
+    dirdcl();
+    while (ns-- > 0)
+        strcat(out, " pointer to");
+}
+
+void dirdcl()
+{
+    int type;
+
+    if (tokentype == '(') { /* ( dcl ) */
+        dcl();
+        if (tokentype != ')') {
+            printf("error: missing )\n");
+        }
+    }
+    else if (tokentype == NAME) { /* variable name */
+        strcpy(name, token);
+    }
+    else {
+        printf("error: expected name or (dcl)\n");
+    }
+
+    while ((type = gettoken()) == PARENS || type == BRACKETS)
+    {
+        if (type == PARENS) {
+            strcat(out, " function returning");
+        }
+        else {
+            strcat(out, " array");
+            strcat(out, token);
+            strcat(out, " of");
+        }
+    }
+}
+
+int gettoken()
+{
+
+}
