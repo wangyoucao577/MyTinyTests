99aebe17138ce54f45fd158db25e6b49c7d1e1ae
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2016-01-24 12:21:05 +0800

    cat version2, errors to stderr.

--- CProgrammingLanguage/simple_cat.c
+++ CProgrammingLanguage/simple_cat.c
@@ -13,19 +13,28 @@ void filecopy(FILE *ifp, FILE *ofp)
 int main(int argc, char *argv[])
 {
 	FILE *fp;
+
+	char * prog = argv[0]; /*program name for errors*/
+
 	if (argc == 1)/*no args; copy standard input*/
 		filecopy(stdin, stdout);
 	else 
 		while (--argc > 0)
 			if ((fp = fopen(*++argv, "r")) == NULL)
 			{
-				printf("cat: can't open %s\n", *argv);
-				return 1;
+				fprintf(stderr, "%s: can't open %s\n", prog, *argv);
+				exit(1);
 			}
 			else
 			{
 				filecopy(fp, stdout);
 				fclose(fp);
 			}
+	if (ferror(stdout))
+	{
+		fprintf(stderr, "%s: error writing stdout\n", prog);
+		exit(2);
+	}
+
 	return 0;
 }
