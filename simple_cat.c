0b73130306b627d3cfaf67d08857d06e6b29dd29
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2016-01-24 12:00:43 +0800

    cat

---
+++ simple_cat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*filecopy : copy file ifp to file ofp*/
+void filecopy(FILE *ifp, FILE *ofp)
+{
+	int c;
+	while ((c = getc(ifp)) != EOF)
+		putc(c, ofp);
+
+}
+
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	if (argc == 1)/*no args; copy standard input*/
+		filecopy(stdin, stdout);
+	else 
+		while (--argc > 0)
+			if ((fp = fopen(*++argv, "r")) == NULL)
+			{
+				printf("cat: can't open %s\n", *argv);
+				return 1;
+			}
+			else
+			{
+				filecopy(fp, stdout);
+				fclose(fp);
+			}
+	return 0;
+}
