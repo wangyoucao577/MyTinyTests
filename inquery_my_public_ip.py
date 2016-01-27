9e67c3f4124b1edcb7eb27fd7f08b6f3fad99eb9
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2016-01-27 10:24:32 +0800

    查询public ip的小软件。

---
+++ inquery_my_public_ip.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2
+
+import re,urllib2
+
+class Getmyip:
+    def getip(self):
+        try:
+            myip = self.visit("http://www.ip138.com/ip2city.asp")
+        except:
+            try:
+                myip = self.visit("http://www.whereismyip.com/")
+            except:
+                myip = "So sorry!!!"
+        return myip
+    def visit(self,url):
+        opener = urllib2.urlopen(url)
+        if url == opener.geturl():
+            str = opener.read()
+            print str + "\n\n"
+            
+        return re.search('\d+\.\d+\.\d+\.\d+',str).group(0)
+getmyip = Getmyip()
+localip = getmyip.getip()
+print localip
