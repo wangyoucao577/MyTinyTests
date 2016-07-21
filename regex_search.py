2e6e0a473082e37d5799ba075bd2c506864a5876
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2016-07-21 16:28:27 +0800

    修改以可作为模块供其他模块集成。

--- regex_search.py
+++ regex_search.py
@@ -20,6 +20,29 @@ from files_operation import *
 #pattern_rule = r'\[curr_.{1,5}kbps_in:[0-9]{1,10}\]'
 #dirpath = "F:\\serial_log\\"
 
+def regex_search(to_check_files_path_list, pattern_rule, print_option):
+    #print "start match, regex pattern--->" + pattern_rule + ", result:"
+    pattern = re.compile(pattern_rule, re.I)
+    matched_count = 0
+    
+    for file_path in to_check_files_path_list:
+        line_number = 0
+        for line in fileinput.input(file_path):
+            line_number += 1
+            match_result = re.search(pattern, line)
+            #print line
+            if match_result:
+                matched_count += 1
+                if print_option == "nothing":
+                    pass
+                elif print_option == "group_0":
+                    print file_path + "(" + str(line_number) + "): " + match_result.group(0)
+                else:
+                    print file_path + "(" + str(line_number) + "): " + line.strip('\n')
+                #print file_path + "(" + str(line_number) + "): " + "value--->" +  match_result.group(1)
+                
+    return matched_count
+    
 def main():
     if (len(sys.argv) < 4):
         print "Usage: "
@@ -37,23 +60,15 @@ def main():
     for suffix in to_check_files_suffix:
         (file_name_list, file_path_list) = scan_folder_to_get_indicated_files(dirpath, suffix)
         to_check_files_path_list += file_path_list
-    print "to check files path: "
-    print to_check_files_path_list
-    print "\n\n"
+    #print "to check files path: "
+    #print to_check_files_path_list
+    #print "\n\n"
     
     #start match
-    print "start match, regex pattern--->" + pattern_rule + ", result:"
-    pattern = re.compile(pattern_rule, re.I)
-    for file_path in to_check_files_path_list:
-        line_number = 0
-        for line in fileinput.input(file_path):
-            line_number += 1
-            match_result = re.search(pattern, line)
-            #print line
-            if match_result:
-                #print file_path + "(" + str(line_number) + "): " + line.strip('\n')
-                print file_path + "(" + str(line_number) + "): " + match_result.group(0)
-                #print file_path + "(" + str(line_number) + "): " + "value--->" +  match_result.group(1)
+    regex_search(to_check_files_path_list, pattern_rule, "line")
 
 if __name__ == '__main__':
     main()
+else:
+    print "Module Loaded: regex_search."
+    
