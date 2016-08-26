2c4f33c99c1d66eac7d9acde55dbf3230b889f13
wangyoucao577 <wangyoucao577@gmail.com>
2016-08-26 13:43:11 +0800

    getaddrinfo的一个bug，只能接受service name输入而不能接受port. 按照stack overflow上的方法fix之.

--- ipv6_to_ipv4/ipv6_to_ipv4/ipv6_to_ipv4/ViewController.m
+++ ipv6_to_ipv4/ipv6_to_ipv4/ipv6_to_ipv4/ViewController.m
@@ -47,19 +47,37 @@
     hints.ai_family = PF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_DEFAULT;
-    error = getaddrinfo(ipv4_str, "http", &hints, &res0);
+    error = getaddrinfo(ipv4_str, NULL, &hints, &res0);
     if (error) {
         errx(1, "%s", gai_strerror(error));
         /*NOTREACHED*/
     }
     s = -1;
     for (res = res0; res; res = res->ai_next) {
+        if (res->ai_family == AF_INET6)
+        {
+            //NOTE!!! getaddrinfo can not specified port(such as "80") as the second parameter service_name, only service_name from /etc/services accept.
+            //Envrionment: iOS 9 and Mac OS X 10.11
+            //This is a bug impacting iOS 9 and Mac OS X 10.11. It has been fixed in iOS 10 and macOS 10.12 (as of beta 1), here is the reference link:
+            //http://stackoverflow.com/questions/37386161/service-port-is-missed-when-using-getaddrinfo-to-convert-ipv4-addr-to-ipv6-addr
+            
+            struct sockaddr_in6 * sockaddr_v6 = (struct sockaddr_in6 *)res->ai_addr;
+            // START WORKAROUND
+            if (sockaddr_v6->sin6_port == 0)
+            {
+                sockaddr_v6->sin6_port = htons(22002);
+            }
+            // END WORKAROUND
+        }
+
+        
         s = socket(res->ai_family, res->ai_socktype,
                    res->ai_protocol);
         if (s < 0) {
             cause = "socket";
             continue;
         }
+        
         
         if (connect(s, res->ai_addr, res->ai_addrlen) < 0) {
             cause = "connect";
