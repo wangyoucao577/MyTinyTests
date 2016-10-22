e92338e4b76ef3fbb5b005a9639567636dac4827
wangyoucao577 <wangyoucao577@gmail.com>
2016-10-22 08:36:58 +0800

    ANSI C中两个相邻的字符串常量自动连接。

---
+++ c_str_auto_connect.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("1111" 
+        "2222\n");
+    printf("%s" "%d" "\n", "test", 123);
+    return 0;
+}
+
+/* vim: set expandtab! tabstop=4 softtabstop=4 shiftwidth=4 number! : */
