b6d1da2b70c90428556b3891d8c33576928e64de
wangyoucao577 <wangyoucao577@gmail.com>
2016-12-02 23:31:09 +0800

    梳理下结构，把city和line提取为变量，便于遍历。

--- get_info_from_maps/amap_get_public_transportations.js
+++ get_info_from_maps/amap_get_public_transportations.js
@@ -1,52 +1,48 @@
+
+//公交线路搜索的回调, 处理查询到的数据
 function lineSearch_Callback(result)
 {
-    var txt = result.info + "</br>";
-    txt += (result.lineInfo.length + "</br>");
+    document.getElementById("result").innerHTML = result.info;
+
+    var stops_txt = "";
+    var path_txt = "";
 
     var lineArr = result.lineInfo;
 
     for (var i = 0; i < lineArr.length; i++) {
         var pathArr = lineArr[i].path;
         var stops = lineArr[i].via_stops;
-        txt += "stops: </br>";
+        //stops_txt += "stops: </br>";
         for (var j = 0; j < stops.length; j++) {
-            txt += (stops[j].location + "," + stops[j].name + "</br>");
+            stops_txt += (stops[j].location + "," + stops[j].name + "</br>");
         }
-        txt += "paths: </br>"
+        //stops_txt += "paths: </br>"
         for (var j = 0; j < pathArr.length; j++) {
-            txt += (pathArr[j].toString() + "</br>")
+            path_txt += (pathArr[j].toString() + "</br>")
         }
     }
-    // var lineArr = data.lineInfo;
-    // var lineNum = data.lineInfo.length;
-    //     if (lineNum == 0) {
-    //     } else {
-    //         for (var i = 0; i < lineNum; i++) {
-    //             var pathArr = lineArr[i].path;
-    //             var stops = lineArr[i].via_stops;
-    //             var startPot = stops[0].location;
-    //             var endPot = stops[stops.length - 1].location;
 
-    //             if (i == 0) drawbusLine(startPot, endPot, pathArr);
-    //         }
-    //     }
-    document.getElementById("id1").innerHTML = txt
+    document.getElementById("stops").innerHTML = stops_txt
+    document.getElementById("path").innerHTML = path_txt
 
 }
-	
+
 //加载公交路线查询服务
 //实例化公交线路查询类，只取回一条路线
-AMap.service(["AMap.LineSearch"], function() {
+AMap.service(["AMap.LineSearch"], function search_line_in_city() {
+
+    var expect_city = '深圳', expect_line = '高快巴士10';
+
     var linesearch = new AMap.LineSearch({
     pageIndex:1,
-    city:'深圳',
+    city: expect_city,
     pageSize:1,
     extensions:'all'//返回全部信息
     });
-    //TODO:调用search方法
-
-    //搜索“536”相关公交线路
-linesearch.search('高快巴士10', function(status, result){
+    
+    //调用search方法
+    //搜索 公交线路
+    linesearch.search(expect_line, function(status, result){
     //取回公交路线查询结果
     if(status === 'complete' && result.info === 'OK'){
                 //取得了正确的公交路线结果
@@ -55,8 +51,9 @@ linesearch.search('高快巴士10', function(status, result){
                 //结果处理函数，demo中绘制了线路
  
     }else{
-        //无数据或者查询失败
- 
+        //无数据或者查询失败, 输出错误信息
+        document.getElementById("result").innerHTML = status + " " + result.info;
     }
 });
 });
+	
