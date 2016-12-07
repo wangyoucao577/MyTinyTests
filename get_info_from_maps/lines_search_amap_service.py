a89cfecf47b522e7aeee8bd2cbd6859d64222838
wangyoucao577 <wangyoucao577@gmail.com>
2016-12-07 23:06:15 +0800

    提取下request的发起及异常处理，作为通用模块。

--- get_info_from_maps/lines_search_amap_service.py
+++ get_info_from_maps/lines_search_amap_service.py
@@ -20,14 +20,41 @@ baidumap_place_search_url = 'http://api.map.baidu.com/place/v2/search?query=å…¬ä
 def print_list(list):
     for i in list:
         print i.encode('utf-8')
+        
+def rest_request(url):
+    request_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
+    request_headers = { 'User-Agent' : request_user_agent, 'Referer': url}
+    try:
+        request = urllib2.Request(url, headers=request_headers)
+        response = urllib2.urlopen(request)
+        content = response.read()#.decode('utf-8')
+        #content = content.encode(sys.getfilesystemencoding())   #for windows default correctly print
+        #print content
+        print "read bytes: " + str(len(content))
+        return content
+
+        #raw_input("Press any key to continue...")
+    except urllib2.HTTPError, e:
+        print "HTTPError Exception:"
+        print e.code
+        print e.reason
+        
+    except urllib2.URLError, e:
+        print "URLError Exception:"
+        if hasattr(e,"code"):
+            print e.code
+        if hasattr(e,"reason"):
+            print e.reason
+        
+    return ""
 
 def nearby_search_for_lines_via_station(city, station, in_out_lines):
     for page in range(1, 100):
+        #TODO:
         pass
 
 def place_search_for_stations_lines(city, base_reqest_url):
 
-    request_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
     
     out_stations = []
     out_lines = []
@@ -35,47 +62,28 @@ def place_search_for_stations_lines(city, base_reqest_url):
     for page in range(1, 100):
         print "request page: " + str(page)
         url = base_reqest_url + "&city=" + city + "&page=" + str(page)
-        request_headers = { 'User-Agent' : request_user_agent, 'Referer': url}
-        try:
-            request = urllib2.Request(url, headers=request_headers)
-            response = urllib2.urlopen(request)
-            content = response.read()#.decode('utf-8')
-            #content = content.encode(sys.getfilesystemencoding())   #for windows default correctly print
-            #print content
-            print "read bytes: " + str(len(content))
-            json_content = json.loads(content)
-
-            print "result.status: " + str(json_content["status"]) + ", result.info: " + json_content["info"]
-            if (0 == json_content["status"] || "OK" != json_content["info"]):
-                print "result failed: " + json_content["info"]
-                break
-            
-            print "result count: " + str(json_content["count"])
-            print "pois len: " + str(len(json_content["pois"]))
-            for a_pois in json_content["pois"]:
-                #print a_pois
-                if a_pois['name'] not in out_stations:
-                    out_stations.append(a_pois['name']);
-                this_pois_lines = a_pois['address'].replace('...', '').split(';')
-                for line in this_pois_lines:
-                    if line not in out_lines:
-                        out_lines.append(line)
-            print "out_stations len: " + str(len(out_stations)) + ", out_lines len: " + str(len(out_lines))
-
-            
-            #raw_input("Press any key to continue...")
-        except urllib2.HTTPError, e:
-            print "HTTPError Exception:"
-            print e.code
-            print e.reason
+        content = rest_request(url)
+        if not content:
+            print "rest request null, break"
             break
-        except urllib2.URLError, e:
-            print "URLError Exception:"
-            if hasattr(e,"code"):
-                print e.code
-            if hasattr(e,"reason"):
-                print e.reason
+        json_content = json.loads(content)
+
+        print "result.status: " + str(json_content["status"]) + ", result.info: " + json_content["info"]
+        if 0 == json_content["status"] or "OK" != json_content["info"]:
+            print "result failed: " + json_content["info"]
             break
+        
+        print "result count: " + str(json_content["count"])
+        print "pois len: " + str(len(json_content["pois"]))
+        for a_pois in json_content["pois"]:
+            #print a_pois
+            if a_pois['name'] not in out_stations:
+                out_stations.append(a_pois['name']);
+            this_pois_lines = a_pois['address'].replace('...', '').split(';')
+            for line in this_pois_lines:
+                if line not in out_lines:
+                    out_lines.append(line)
+        print "out_stations len: " + str(len(out_stations)) + ", out_lines len: " + str(len(out_lines))
             
     return (out_stations, out_lines)
 
