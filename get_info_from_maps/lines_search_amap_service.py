5ed5ac7c1d41bb800f97dca65d12a704c1442677
wangyoucao577 <wangyoucao577@gmail.com>
2016-12-08 10:19:46 +0800

    通过高德的Web服务API进行POI搜索。但由于其限制每天只能查询1000次，就不能迭代了。

--- get_info_from_maps/lines_search_amap_service.py
+++ get_info_from_maps/lines_search_amap_service.py
@@ -13,9 +13,13 @@ import urllib2
 import re
 import sys
 import json
+import time
 
 amap_place_search_url = 'http://restapi.amap.com/v3/place/text?key=ec001ba28cef4168991d5dc2063ea2d1&types=150700&extensions=all&children=1'
+amap_nearby_search_url = 'http://restapi.amap.com/v3/place/around?key=ec001ba28cef4168991d5dc2063ea2d1&types=150700&extensions&children=1&radius=50000'
+
 baidumap_place_search_url = 'http://api.map.baidu.com/place/v2/search?query=公交车站&tag=交通设施&page_size=20&page_num=0&scope=1&region=上海&output=json&ak=GvqrFr3Ns8eaHG4bMUPX4yN4R0jZ8oDq'
+
 
 def print_list(list):
     for i in list:
@@ -48,15 +52,42 @@ def rest_request(url):
         
     return ""
 
-def nearby_search_for_lines_via_station(city, station, in_out_lines):
+def nearby_search_for_lines_via_location(base_reqest_url, city, location, in_out_stations, in_out_stations_location, in_out_lines):
     for page in range(1, 100):
-        #TODO:
-        pass
+        print "request page: " + str(page)
+        url = base_reqest_url + "&city=" + city + "&page=" + str(page) + "&location=" + location
+        content = rest_request(url)
+        if not content:
+            print "rest request null, break"
+            break
+        json_content = json.loads(content)
+
+        print "result.status: " + str(json_content["status"]) + ", result.info: " + json_content["info"]
+        if 0 == json_content["status"] or "OK" != json_content["info"]:
+            print "result failed: " + json_content["info"]
+            break
+
+        print "result count: " + str(json_content["count"])
+        print "pois len: " + str(len(json_content["pois"]))
+        for a_pois in json_content["pois"]:
+            if a_pois['name'] not in in_out_stations:
+                in_out_stations.append(a_pois['name'])
+            if a_pois['location'] not in in_out_stations_location:
+                in_out_stations_location.append(a_pois['location'])
+            this_pois_lines = a_pois['address'].replace('...', '').split(';')
+            for line in this_pois_lines:
+                if line not in in_out_lines:
+                    in_out_lines.append(line)
+
+        print "out_stations len: " + str(len(in_out_stations)) + ", out_stations_location len: " + str(len(in_out_stations_location)) + ", out_lines len: " + str(len(in_out_lines))
+
+    return (in_out_stations, in_out_stations_location, in_out_lines)
 
 def place_search_for_stations_lines(city, base_reqest_url):
 
     
     out_stations = []
+    out_stations_location = []
     out_lines = []
     
     for page in range(1, 100):
@@ -79,18 +110,53 @@ def place_search_for_stations_lines(city, base_reqest_url):
             #print a_pois
             if a_pois['name'] not in out_stations:
                 out_stations.append(a_pois['name']);
+            if a_pois['location'] not in out_stations_location:
+                out_stations_location.append(a_pois['location'])
+
             this_pois_lines = a_pois['address'].replace('...', '').split(';')
             for line in this_pois_lines:
                 if line not in out_lines:
                     out_lines.append(line)
-        print "out_stations len: " + str(len(out_stations)) + ", out_lines len: " + str(len(out_lines))
+
+        print "out_stations len: " + str(len(out_stations)) + ", out_stations_location len: " + str(len(out_stations_location)) + ", out_lines len: " + str(len(out_lines))
             
-    return (out_stations, out_lines)
+    return (out_stations, out_stations_location, out_lines)
 
 def main():
-    (stations, lines) = place_search_for_stations_lines('上海', amap_place_search_url)
-    print_list(stations)
-    print_list(lines)
+    city = '021'
+
+    tick1 = time.time();
+    print "tick1: " + str(tick1);
+
+    (stations, stations_location, lines) = place_search_for_stations_lines(city, amap_place_search_url)
+    #print_list(stations)
+    #print_list(stations_location)
+    #print_list(lines)
+    print "len(stations): " + str(len(stations)) + "len(stations_location): " + str(len(stations_location)) + ", len(lines): " + str(len(lines))
+    
+    tick2 = time.time();
+    print "tick2: " + str(tick2) + ", place_search cost seconds: " + str(tick2 - tick1) + "."
+
+    # print "ready to search nearby."
+    # stations_location_copy
+    # stations_location_copy = stations_location
+    # for x in stations_location_copy:
+    #     (stations, stations_location, lines) = nearby_search_for_lines_via_location(amap_nearby_search_url, city, x, stations, stations_location, lines)
+    # print "len(stations): " + str(len(stations)) + "len(stations_location): " + str(len(stations_location)) + ", len(lines): " + str(len(lines))
+    #
+    # tick3 = time.time();
+    # print "tick3: " + str(tick3) + ", nearby search cost seconds: " + str(tick3 - tick2) + ", ready for nearby search again."
+    #
+    #  = stations_location
+    # for x in stations_location_copy:
+    #     (stations, stations_location, lines) = nearby_search_for_lines_via_location(amap_nearby_search_url, city, x, stations, stations_location, lines)
+    #
+    # print_list(lines)
+    # print "len(stations): " + str(len(stations)) + "len(stations_location): " + str(len(stations_location)) + ", len(lines): " + str(len(lines))
+    #
+    # tick4 = time.time();
+    # print "tick4: " + str(tick4) + ", nearby search again cost seconds: " + str(tick4 - tick3) + ", app total cost seconds: " + str(tick4 - tick1)
+
 
 if __name__ == '__main__':
     main()
