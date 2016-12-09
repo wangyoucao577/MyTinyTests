a656aba044decf58959d9f3a6a1acc82b10824ae
wangyoucao577 <wangyoucao577@gmail.com>
2016-12-09 16:49:18 +0800

    支持placesearch的city_lines也写入文件。

--- get_info_from_maps/write_to_file.js
+++ get_info_from_maps/write_to_file.js
@@ -1,10 +1,10 @@
 
 
 //申请空间, 线路的搜索结果写入文件
-function write_to_file(city, out_str)
+function write_to_file(city, type, out_str)
 {
     var now_str = (new Date()).Format("yyyyMMdd_HHmmss")
-    var new_file = city + "_LinesDetails_" + now_str + ".txt";
+    var new_file = city + "_" + type + "_" + now_str + ".txt";
 
     //申请空间, save to File
     window.webkitRequestFileSystem(window.TEMPORARY, 100*1024*1024, function onInitFs(fs) {
@@ -16,7 +16,7 @@ function write_to_file(city, out_str)
                 writer.onwriteend = function(e) {
                     console.log('write complete, file:' + new_file);
                 };
-                writer.write(new Blob([out_str.replace(/<\/br>/g, "\n")], {type: 'text/plain'}));
+                writer.write(new Blob([out_str], {type: 'text/plain'}));
             });
       }, errorHandler);
     }, errorHandler);
