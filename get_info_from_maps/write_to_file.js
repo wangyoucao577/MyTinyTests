5e437cab416fef47d465a464640133da8a726bf2
wangyoucao577 <wangyoucao577@gmail.com>
2016-12-09 16:56:53 +0800

    refactor，以及添加nearby结果写入文件的功能（暂无法测试，由于高德的拖动验证问题）

--- get_info_from_maps/write_to_file.js
+++ get_info_from_maps/write_to_file.js
@@ -22,3 +22,18 @@ function write_to_file(city, type, out_str)
     }, errorHandler);
 }
 
+function write_city_lines_to_file(city, type, city_lines){
+
+    //format
+    var out_str = "count = " + city_lines.length + "\nvar city_lines = [";
+    for (var i in city_lines){
+        out_str += "\'" + city_lines[i] + "\'";
+        if (i < city_lines.length - 1){
+            out_str += ", ";
+        }
+    }
+    out_str += "]";
+
+    //把结果写入文件
+    write_to_file(city, type, out_str);
+}
