497436fd78816266aac2f1c13f74c7848d3b7a57
wangyoucao577 <wangyoucao577@gmail.com>
2016-12-11 23:06:54 +0800

    支持分析baiduMap localsearch的结果。

--- get_info_from_maps/local_search_baidumap.js
+++ get_info_from_maps/local_search_baidumap.js
@@ -1,32 +1,33 @@
 
 var local_search_global_bmap = {};
 
-local_search_global_bmap.localSearch_Callback = function (result) {
-    
-  console.log(result);
-  // if (local.getStatus() == BMAP_STATUS_SUCCESS){      
-  //       // 判断状态是否正确      
-  //       // var s = [];      
-  //       // for (var i = 0; i < results.getCurrentNumPois(); i ++){      
-  //       //     s.push(results.getPoi(i).title + ", " + results.getPoi(i).address);      
-  //       // }      
-  // } else {
-    
-  // }     
-}
-
-//入口
-
-
 local_search_global_bmap.execute_local_search = function (city, place){
 
-  var options = {      
-      onSearchComplete: local_search_global_bmap.localSearch_Callback,    
-      pageCapacity:100
-  };      
+  var local = new BMap.LocalSearch(city, {      
+      onSearchComplete: function (result) {
+          //console.log(result);
+          console.log("NumPages: " + result.getNumPages() + ", NumPois: " + result.getNumPois() 
+            + ", CurrentNumPois: " + result.getCurrentNumPois() + ", PageIndex: " + result.getPageIndex());
 
-  var local = new BMap.LocalSearch(city, options);     
-  local.search(place);
+          
+
+          for (var i = 0; i < result.getCurrentNumPois(); ++i){
+            var poi = result.getPoi(i);
+            console.log("local poi result " + i + ", title: " + poi.title + ", location: " + poi.point.lng + "," + poi.point.lat
+              + ", address: " + poi.address);
+          }
+
+          //后续还有结果页面, 应翻页
+          if (result.getPageIndex() + 1 < result.getNumPages()){
+            local.gotoPage(result.getPageIndex() + 1);
+          }else{
+            console.log("all pages completed");
+          }
+
+      },    
+      pageCapacity:100
+  });     
+  local.search(place, {forceLocal:true});
 
 }
 
