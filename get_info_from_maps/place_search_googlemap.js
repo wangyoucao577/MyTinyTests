ea438d7ebb2da8cd0a78b85bc5eabb2ec60702e8
wangyoucao577 <wangyoucao577@gmail.com>
2016-12-29 16:51:43 +0800

    googlemap加上getDetails动作。

--- get_info_from_maps/place_search_googlemap.js
+++ get_info_from_maps/place_search_googlemap.js
@@ -1,3 +1,8 @@
+/**
+ * @file place search function on Google map
+ * @author wangyoucao577(wangyoucao577@gmail.com)
+ */
+
 
 //NOTE: 与BaiduMap和AMap不同, GoogleMap的JavaScript API貌似并未提供通过公交站点的公交线路.
 //      代码先留着, 有时需要测试用. 几种placeSearch调用方式都差不多.
@@ -8,11 +13,24 @@
 
 var place_search_global_gmap = {};
 
+place_search_global_gmap.execute_get_details = function (service, place_id) {
+    service.getDetails({
+        placeId: place_id
+    }, 
+    function (result, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            console.log("name: " + result.name + ", place_id: " + result.place_id + ", vicinity: " + result.vicinity + "\n");
+        }else{
+            console.log("getDetails failed, status: " + status);
+        }
+    });
+}
+
 
 place_search_global_gmap.execute_nearby_search = function (all_done_callback, city, map_provider_name){
 
     //TODO: could transfer city to location
-    var pyrmont = new google.maps.LatLng(31.225454, 121.471344);
+    var pyrmont = new google.maps.LatLng(31.1770572,121.4232641);
 
     var map = new google.maps.Map(document.createElement('map'), {
         center: pyrmont,
@@ -20,11 +38,12 @@ place_search_global_gmap.execute_nearby_search = function (all_done_callback, ci
     });
 
     var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        keyword: '公交',
-        location: pyrmont,
-        radius: '50000',
-        type: 'bus_station'
+    service.textSearch({
+            //keyword: '公交',
+            query: '地铁站',
+            location: pyrmont,
+            radius: '50000',
+            //type: 'bus_station'
         },
         function (results, status){
               console.log(status);
@@ -32,8 +51,9 @@ place_search_global_gmap.execute_nearby_search = function (all_done_callback, ci
                   for (var i = 0; i < results.length; ++i) {
                       console.log("index: " + i + ", name: " + results[i].name + ", place_id: " + results[i].place_id + ", vicinity: " +  results[i].vicinity + "\n");
                       //TODO: for GoogleMap
-                  }
 
+                      place_search_global_gmap.execute_get_details(service, results[i].place_id);
+                  }
               }
         }
     );
