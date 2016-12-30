d3bdcfdf40baea752a65b27f26ab807dea8aebe2
wangyoucao577 <wangyoucao577@gmail.com>
2016-12-30 16:53:09 +0800

    添加生成 search_param_in 的py, 用于合并几个搜寻的文件为一个, 过滤重复的。 待实现。

---
+++ get_info_from_maps/generate_search_param_in.py
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+"""
+Description: 合并各个渠道(AMap/BaiduMap PlaceSearch迭代, wiki城市公交列表等)输出的文件, 生成定义的search_param_in.js文件, 供后续搜索使用
+Author:     wangyoucao577@gmail.com
+Created Date:   2016-12-30
+Version:        2016-12-30
+"""
+
+import sys
+sys.path.append(sys.path[0] + r'\..')   #上级目录中加入搜寻列表, 以便于导入上级目录中的py模块
+from files_operation import *
+
+def main():
+    pass
+            
+if __name__ == '__main__':
+    main()
+    
+
+            
