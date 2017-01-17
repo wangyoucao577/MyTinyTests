b96e03ec330a5602b6dbce041a33bf8cacb95172
wangyoucao577 <wangyoucao577@gmail.com>
2017-01-17 15:23:17 +0800

    test: 添加循环的test case, 比较cpu占用率

--- mars_test/xlog_test/src/xlog_test.cc
+++ mars_test/xlog_test/src/xlog_test.cc
@@ -7,10 +7,12 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
 
 #import <mars/xlog/xloggerbase.h>
 #import <mars/xlog/xlogger.h>
 #import <mars/xlog/appender.h>
+#import <mars/comm/time_utils.h>
 
 static void test_case_each_xlog_interface(){
     xverbose2("%s %d %s", "verbose my test", 1, "1");
@@ -23,6 +25,38 @@ static void test_case_each_xlog_interface(){
     xerror2(TSF"%_ %_ %_", "error my test", 3, "3");
     xfatal2(TSF"%_ %_ %_", "fatal my test", 3, "3");
 
+}
+
+static void test_case_loop_xlog(int xlog_count_per_sec){
+    
+    int loop_index = 0;
+    
+    int curr_xlog_count = 0;
+    
+    uint64_t curr_interval_start_ms = timeMs();
+    uint64_t curr_ms = 0;
+    
+    int interval_count = 0;
+    
+    
+    while (1){
+        
+        curr_ms = timeMs();
+        if (curr_ms >= curr_interval_start_ms + 1000){
+            ++interval_count;
+            
+            curr_xlog_count = 0;
+            curr_interval_start_ms = curr_ms;
+            continue;
+        }
+        
+        if (curr_xlog_count >= xlog_count_per_sec){
+            usleep(10 * 1000);
+            continue;
+        }
+            
+        xverbose2(TSF"%_ %_ curr_interval_index %_ interval %_", "verbose", loop_index++, curr_xlog_count++, interval_count);
+    }
 }
 
 
@@ -50,4 +84,6 @@ void destory_xlog_test()
 void run_xlog_test_cases()
 {
     test_case_each_xlog_interface();
+    
+    test_case_loop_xlog(1000);
 }
