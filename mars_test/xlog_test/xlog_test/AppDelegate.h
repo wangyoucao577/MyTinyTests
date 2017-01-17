f862b2df9d2c1f2e1859f1c2db5c6f2bfcf42b2e
wangyoucao577 <wangyoucao577@gmail.com>
2017-01-17 10:36:39 +0800

    chore: test pro

---
+++ mars_test/xlog_test/xlog_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xlog_test
+//
+//  Created by zhangjie on 1/17/17.
+//  Copyright © 2017 wangyoucao577. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mars_test/xlog_test/xlog_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xlog_test
+//
+//  Created by zhangjie on 1/17/17.
+//  Copyright © 2017 wangyoucao577. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
