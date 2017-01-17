a9985cbe01f5ef29cad19be35755deb0e208246e
wangyoucao577 <wangyoucao577@gmail.com>
2017-01-17 10:47:04 +0800

    chore: 提交下blog的build _[2017-01-17_10.41]@git-bb793a0

---
+++ mars_test/lib/mars-log-iphone/mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "bb793a0"
+#define MARS_PATH "master"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2017-01-17 10:41:15"
+#define MARS_TAG "_[2017-01-17_10.41]@git-bb793a0@zhangj"
+
+#endif
