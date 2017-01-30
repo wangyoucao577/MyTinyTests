d91f51ee552d32f49bc0cc833c751dca1383e6df
wangyoucao577 <wangyoucao577@gmail.com>
2017-01-30 22:44:46 +0800

    chore(cc11): 添加 sizeof test 的测试工程

---
+++ cc11/sizeof_test/sizeof_test.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+#if defined(_MSC_VER)
+    //wait before return
+    cout << "Please press any key to continue..." << endl;
+    char a;
+    cin >> a;
+#endif
+}
