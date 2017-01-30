e7cdcd9fb12d7f9ed38cbabab130fde4a0d25d40
wangyoucao577 <wangyoucao577@gmail.com>
2017-01-30 22:49:15 +0800

    feat(cc11): 添加 C++11 中 sizeof 扩展的用法尝试

--- cc11/sizeof_test/sizeof_test.cc
+++ cc11/sizeof_test/sizeof_test.cc
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
+struct People {
+    int hand;
+    static People* all;
+};
+
 int main()
 {
+    People p;
+    cout << "sizeof(p.hand): " << sizeof(p.hand) << endl;
+    cout << "sizeof(p.all): " << sizeof(p.all) << endl;
+    cout << "sizeof(People::all): " << sizeof(People::all) << endl;
+    cout << "sizeof(People::hand): " << sizeof(People::hand) << endl; //C++11最新支持
+    cout << "sizeof(((People*)0)->hand): " << sizeof(((People*)0)->hand) << endl; //C++11前的技巧
+
 #if defined(_MSC_VER)
     //wait before return
     cout << "Please press any key to continue..." << endl;
