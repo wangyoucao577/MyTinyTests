3b81ef1cbe6dfc77ce3861a58673adbd24160d42
wangyoucao577 <wangyoucao577@gmail.com>
2017-01-30 22:06:51 +0800

    chore(cc11): 添加 就地初始化 的测试项目

---
+++ cc11/inclass_initialization_test/inclass_initialization_test.cc
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+#if defined(_MSC_VER)
+    //wait before return
+    cout << "Please press any key to continue..." << endl;
+    char a;
+    cin >> a;
+#endif
+    return 0;
+
+}
