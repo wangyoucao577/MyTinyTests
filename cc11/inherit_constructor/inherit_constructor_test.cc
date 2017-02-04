f5e0bc7b7a8ce4ae37c81433f8782ca3c90393a4
wangyoucao577 <wangyoucao577@gmail.com>
2017-02-04 15:30:29 +0800

    chore(cc11): 继承的小实验

--- cc11/inherit_constructor/inherit_constructor_test.cc
+++ cc11/inherit_constructor/inherit_constructor_test.cc
@@ -2,8 +2,33 @@
 #include <iostream>
 using namespace std;
 
+class MyBase {
+public:
+    void PrintMyBase() {
+        cout << "Call My Base, a_ = " << a_ << endl;
+    }
+
+private:
+    int a_ { 10 };
+};
+
+class InheritOne : MyBase {
+public:
+    void PrintOne() {
+        PrintMyBase();
+        cout << "Inherit One from My Base, b_ = " << b_ << endl;
+    }
+
+private:
+    char b_ {'b'};
+};
+
 int main()
 {
+    InheritOne aOne;
+    aOne.PrintOne();
+    //aOne.PrintMyBase();   //默认为private继承，故派生类的对象无法直接访问基类的成员和方法, 包括基类的public方法
+
 #if defined(_MSC_VER)
     //wait before return
     cout << "Please press any key to continue..." << endl;
