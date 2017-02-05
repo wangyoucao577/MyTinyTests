43d7b3ea4171d7729ec1590fc60cd577f3cc8136
wangyoucao577 <wangyoucao577@gmail.com>
2017-02-05 14:39:16 +0800

    feat(cc11): 添加 判断是否是引用的几个小工具示例

--- cc11/rvalue_reference/UnitTest.cc
+++ cc11/rvalue_reference/UnitTest.cc
@@ -2,7 +2,11 @@
 #include "HasPtrMem.h"
 
 #include <iostream>
+#include <string>
+#include <type_traits>
 using namespace std;
+
+#define VNAME(e) (#e)
 
 UnitTest::UnitTest()
 {
@@ -17,6 +21,7 @@ void UnitTest::Run()
 {
     //TestCase1();
     TestCase2();
+    TestCase3();
 }
 
 void UnitTest::TestCase1()
@@ -32,4 +37,33 @@ void UnitTest::TestCase2()
     HasPtrMem a = HasPtrMem::GetTemp();
 }
 
+void UnitTest::TestCase3()
+{
+    cout << endl;
 
+    cout << VNAME(is_reference<int>::value) << " " << is_reference<int>::value << endl;
+    cout << VNAME(is_reference<int&>::value) << " " << is_reference<int&>::value << endl;
+    cout << VNAME(is_reference<const int&>::value) << " " << is_reference<const int&>::value << endl;
+    cout << VNAME(is_reference<int&&>::value) << " " << is_reference<int&&>::value << endl;
+    cout << VNAME(is_reference<const int&&>::value) << " " << is_reference<const int&&>::value << endl;
+    cout << VNAME(is_reference<int*>::value) << " " << is_reference<int*>::value << endl;
+
+    cout << endl;
+
+    cout << VNAME(is_rvalue_reference<int>::value) << " " << is_rvalue_reference<int>::value << endl;
+    cout << VNAME(is_rvalue_reference<int&>::value) << " " << is_rvalue_reference<int&>::value << endl;
+    cout << VNAME(is_rvalue_reference<const int&>::value) << " " << is_rvalue_reference<const int&>::value << endl;
+    cout << VNAME(is_rvalue_reference<int&&>::value) << " " << is_rvalue_reference<int&&>::value << endl;
+    cout << VNAME(is_rvalue_reference<const int&&>::value) << " " << is_rvalue_reference<const int&&>::value << endl;
+    cout << VNAME(is_rvalue_reference<int*>::value) << " " << is_rvalue_reference<int*>::value << endl;
+    
+    cout << endl;
+
+    cout << VNAME(is_lvalue_reference<int>::value) << " " << is_lvalue_reference<int>::value << endl;
+    cout << VNAME(is_lvalue_reference<int&>::value) << " " << is_lvalue_reference<int&>::value << endl;
+    cout << VNAME(is_lvalue_reference<const int&>::value) << " " << is_lvalue_reference<const int&>::value << endl;
+    cout << VNAME(is_lvalue_reference<int&&>::value) << " " << is_lvalue_reference<int&&>::value << endl;
+    cout << VNAME(is_lvalue_reference<const int&&>::value) << " " << is_lvalue_reference<const int&&>::value << endl;
+    cout << VNAME(is_lvalue_reference<int*>::value) << " " << is_lvalue_reference<int*>::value << endl;
+
+}
--- cc11/rvalue_reference/UnitTest.h
+++ cc11/rvalue_reference/UnitTest.h
@@ -11,5 +11,6 @@ public:
 public:
     void TestCase1();
     void TestCase2();
+    void TestCase3();
 };
 
