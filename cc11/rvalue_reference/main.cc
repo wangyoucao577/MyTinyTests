1a069c32751a4530d4154d02bf2a4edc4e48de0f
wangyoucao577 <wangyoucao577@gmail.com>
2017-02-05 10:45:51 +0800

    chore(cc11): 添加 右值引用 的测试项目

---
+++ cc11/rvalue_reference/main.cc
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //TODO: test codes
+
+#if defined(_MSC_VER)
+    //wait before return
+    cout << "Please press any key to continue..." << endl;
+    char a;
+    cin >> a;
+#endif
+
+}
