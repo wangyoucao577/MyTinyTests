91656718c99266406a4d0978638f086c6a9fba4c
wangyoucao577 <wangyoucao577@gmail.com>
2017-02-15 17:48:55 +0800

    feat(cc11): 添加简单的 SFIENA 示例代码

--- cc11/SFINEA/main.cc
+++ cc11/SFINEA/main.cc
@@ -3,9 +3,31 @@
 
 #include "comm_include.h"
 
+
+struct TestA {
+    using foo = int;
+};
+
+template<typename T>
+void f(typename T::foo) { cout << "void f(typename T::foo) " << "called. " << endl; }
+
+template<typename T>
+void f(T) { cout << "void f(T) " << "called. " << endl; }
+
+void TestCase1() {
+    ENTER_FUNC;
+
+    f<TestA>(10);   //∆•≈‰ void f(typename T::foo)
+    f<int>(10); //∆•≈‰ void f(T)
+
+    EXIT_FUNC;
+}
+
+
 int main() {
 
-    //TODO: test codes
+    // test codes
+    TestCase1();
 
     ROUTINE_BEFORE_EXIT_MAIN_ON_WINOWS;
 }
