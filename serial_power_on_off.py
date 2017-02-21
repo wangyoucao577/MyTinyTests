38002caa079ae421e3aaf21de59f9e5e71b51a9f
wangyoucao577 <wangyoucao577@gmail.com>
2017-02-21 23:14:18 +0800

    serial_power_on_off 脚本同时支持python2和python3调用, 方便使用
    - sys.stdout.write 替代print
    - tab全部转成space, 兼容性好一些

--- serial_power_on_off.py
+++ serial_power_on_off.py
@@ -21,28 +21,30 @@ power_off_cmd = [0x55, 0x01, 0x13, 0x00, 0x00, 0x00, 0x00, 0x69]
 ####################################################################
 
 def scan_comports_out():
-	comport_list = list(serial.tools.list_ports.comports())
-	comport_name_list = []
-	#sys.stdout.write("Available Serial Ports: ")
-	for com in comport_list:
-		comport_name_list.append(list(com)[0])
-		#sys.stdout.write(list(com)[0] + ' ')
-	#sys.stdout.write("\n")
-	return comport_name_list
+    comport_list = list(serial.tools.list_ports.comports())
+    comport_name_list = []
+    #sys.stdout.write("Available Serial Ports: ")
+    for com in comport_list:
+        comport_name_list.append(list(com)[0])
+        #sys.stdout.write(list(com)[0] + ' ')
+    #sys.stdout.write("\n")
+    return comport_name_list
 
-	
+    
 def power_operate_com(comport, cmd):
-	try:
-		serialPort = serial.Serial(comport)
-		#print(cmd)
-		serialPort.write(cmd)
-		serialPort.close()
-	except:
-		print("Error: Can not open serial port ", comport)
-		#traceback.print_exc()
-		
+    try:
+        serialPort = serial.Serial(comport)
+        #print(cmd)
+        serialPort.write(cmd)
+        serialPort.close()
+    except:
+        sys.stdout.write("Error: Can not open serial port " + str(comport) + "\n")
+        #traceback.print_exc()
+        
 #Working start
 def main():
+    #sys.stdout.write("sys.version: " + str(sys.version) + "\n")  
+
     available_comports = scan_comports_out()
     available_commands = ["ON", "OFF"]
     operation_commands = {}
@@ -54,9 +56,9 @@ def main():
 
     #print(sys.argv)
     if (len(sys.argv) < 3 or (sys.argv[1] not in available_comports) or (sys.argv[2].upper() not in available_commands)) :
-        print("Usage: ")
-        print("      python serial_power_on_off.py [COM1|COM2|...] [ON|OFF]")
-        print("Available Serial Ports: ", available_comports)
+        sys.stdout.write("Usage: \n")
+        sys.stdout.write("      python serial_power_on_off.py [COM1|COM2|...] [ON|OFF]\n")
+        sys.stdout.write("Available Serial Ports: " + str(available_comports) + "\n")
     else:
         power_operate_com(sys.argv[1], operation_commands[sys.argv[2].upper()])
         
