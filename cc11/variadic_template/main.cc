539154f58fe368e9cbaf4efee4804dd484806828
wangyoucao577 <wangyoucao577@gmail.com>
2017-03-15 09:44:16 +0800

    feat(cc11): 添加类型变长模板 和 非类型变长模板 的示例代码

--- cc11/variadic_template/main.cc
+++ cc11/variadic_template/main.cc
@@ -1,9 +1,50 @@
 
 #include "comm_include.h"
 
+//变长模板的声明
+template <typename... Elements> 
+class MyTuple;
+
+//变长模板的递归特化
+template <typename Head, typename... Tail>
+class MyTuple<Head, Tail...> : private MyTuple<Tail...> {
+    Head head;
+};
+//边界条件
+template<> class MyTuple<>{};
+
+//非类型变长模板声明
+template <int ... A>
+class NonTypeVariadicTemplate;
+
+//非类型变长模板的递归特化
+template <int first, int ... last>
+class NonTypeVariadicTemplate<first, last...> {
+public:
+    static const long val = first * NonTypeVariadicTemplate<last...>::val;
+};
+
+//边界条件
+template<>
+class NonTypeVariadicTemplate<> {
+public:
+    static const long val = 1;
+};
+
+void TestCase1() {
+    ENTER_FUNC;
+
+    MyTuple<int, char, double> mt;
+    NonTypeVariadicTemplate<1, 10, 100> ntvt;
+    cout << NonTypeVariadicTemplate<2, 5, 10, 15>::val << endl;
+
+    EXIT_FUNC;
+}
+
 int main() {
 
-    //TODO: test codes
+    // test codes
+    TestCase1();
 
     ROUTINE_BEFORE_EXIT_MAIN_ON_WINOWS;
 }
