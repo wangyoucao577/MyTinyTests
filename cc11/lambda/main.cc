f33e9362ba0206352a28a09901577b529b07fe11
wangyoucao577 <wangyoucao577@gmail.com>
2017-03-24 16:56:16 +0800

    feat(cc11): 添加lambda的几个示例代码

--- cc11/lambda/main.cc
+++ cc11/lambda/main.cc
@@ -16,7 +16,7 @@ void TestCase1() {
     auto sum2 = [&] {return a + b; };
     cout << sum2() << endl;
 
-    auto sum3 = [=, &b] {return a + b; };   //按值传递时, 是定义的时候直接传值? 
+    auto sum3 = [=, &b] {return a + b; };   //按值传递时, 是定义的时候直接传值 
     a = 5;
     b = 10;
     cout << sum3() << endl;
@@ -41,10 +41,116 @@ void TestCase1() {
     EXIT_FUNC;
 }
 
+int z = 0;
+float c = 0.0;
+void TestCase2() {
+    ENTER_FUNC;
+    int x, y = 3;
+    float a, b = 4.0;
+
+    int success = 0;
+    
+    auto validate = [&]() -> bool {
+        if ((x == y + z) && (a == b + c)) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    };
+
+    success += validate();
+
+    y = 1024;
+    b = 1e13;
+    success += validate();
+
+    cout << success << endl;
+    EXIT_FUNC;
+}
+
+void TestCase3() {
+    ENTER_FUNC;
+    int times = 100;
+
+    int x = 0;
+    for (int i = 0; i < times; ++i)
+    {
+        x += 10;
+    }
+
+    const int y = [=] {   //与x完全等同的赋值, 定义时直接调用. 但此种赋值可以返回给const. 
+        int val = 0;
+        for (int i = 0; i < times; ++i)
+        {
+            val += 10;
+        }
+        return val;
+    }();
+
+    cout << x << ", " << y << endl;
+    EXIT_FUNC;
+}
+
+void TestCase4() {
+    ENTER_FUNC;
+
+    int j = 12;
+    auto by_val_lambda = [=] {return j + 1; };  //按值传递, 函数定义时决定捕获值
+    auto by_ref_lambda = [&] {return j + 1; };  //按引用传递, 函数调用时决定捕获值
+
+    cout << "by_val_lambda: " << by_val_lambda() << endl;
+    cout << "by_ref_lambda: " << by_ref_lambda() << endl;
+
+    j++;
+    cout << "by_val_lambda: " << by_val_lambda() << endl;
+    cout << "by_ref_lambda: " << by_ref_lambda() << endl;
+
+    EXIT_FUNC;
+}
+
+void TestCase5() {
+    ENTER_FUNC;
+
+    int girls = 3, boys = 4;
+    auto totalChild = [](int x, int y) {return x + y; };
+    cout << typeid(totalChild).name() << endl;
+
+    typedef int(*allChild)(int, int);
+    allChild p = totalChild;    //lambda允许赋值给同样定义的函数指针(lambda不能有捕获)
+
+    decltype(totalChild) p2 = totalChild;   
+    //decltype(totalChild) p3 = p;  //但函数指针不能赋值给lambda类型变量
+
+
+    int val = 5;
+    cout << val << endl;
+
+    //默认lambda为const, 不允许修改按值捕获的变量
+    //auto const_val_lambda = [=] {val = 3; };
+    
+    //加上mutable修饰后, 取消了const属性, 但修改的值不会传出
+    auto mutable_val_lambda = [=]() mutable { val = 6; };
+    mutable_val_lambda();
+    cout << val << endl;
+
+    //按引用传递, 可以在内部修改捕获的值, 并传出. const修饰的是引用本身. 
+    auto const_ref_lambda = [&] { val = 7; };
+    const_ref_lambda();
+    cout << val << endl;
+
+
+    EXIT_FUNC;
+}
+
 int main() {
 
     // test codes
     TestCase1();
+    TestCase2();
+    TestCase3();
+    TestCase4();
+    TestCase5();
 
     ROUTINE_BEFORE_EXIT_MAIN_ON_WINOWS;
     return 0;
