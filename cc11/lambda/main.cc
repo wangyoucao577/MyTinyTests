a4e0d518d2b57775f670efc042b8309fabba6318
wangyoucao577 <wangyoucao577@gmail.com>
2017-03-24 11:33:54 +0800

    chore(cc11): 修订下lambda的示例代码

--- cc11/lambda/main.cc
+++ cc11/lambda/main.cc
@@ -5,6 +5,11 @@ void TestCase1() {
     ENTER_FUNC;
 
     int a = 3, b = 4;
+
+    auto var_print = [&] {
+        cout << "a = " << a << ", b = " << b << endl;
+    };
+
     auto sum1 = [](int x, int y)->int {return x + y; };
     cout << sum1(a, b) << endl;
 
@@ -15,15 +20,23 @@ void TestCase1() {
     a = 5;
     b = 10;
     cout << sum3() << endl;
+    var_print();
 
-    auto sum4_plusplus = [=, &b]() mutable {
-        a++;
+    auto sum4_plusplus = [=, &b]() {
         b++;
         return a + b;
     };
     cout << sum4_plusplus() << endl;
+    var_print();
 
-    cout << a << ", " << b << endl;
+    auto sum5_plusplus = [=, &b]() mutable {
+        a++;    //加mutable可以修改按值捕获变量, 但依然值不会传出. mutable还是应该用在成员函数中
+        b++;
+        return a + b;
+    };
+    cout << sum5_plusplus() << endl;
+
+    var_print();
 
     EXIT_FUNC;
 }
