86c4003e239e100831c5a60e88bcd773baf68eb5
wangyoucao577 <wangyoucao577@gmail.com>
2017-05-16 18:08:56 +0800

    初步测试代码

--- RESTClientSample/RESTClientSample/json_samples.h
+++ RESTClientSample/RESTClientSample/json_samples.h
@@ -1,7 +1,13 @@
 #pragma once
 
+// jsoncpp sample
 void unserialize_sample_1();
 void unserialize_sample_2();
 void serialize_sample_1();
 void serialize_sample_2(char * buff, int buff_len);
 void json_samples();
+
+
+// rapidjson sample
+void rapidjson_sample();
+
