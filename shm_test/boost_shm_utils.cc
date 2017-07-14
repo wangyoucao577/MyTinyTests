4b0b24f919c2775e1f168fff217a1a9e758a57ae
wangyoucao577 <wangyoucao577@gmail.com>
2017-07-14 16:25:21 +0800

    add boost_xxx.h/cc for boost shm interface test, @todo

---
+++ shm_test/boost_shm_utils.cc
@@ -0,0 +1,6 @@
+
+#include "boost_shm_utils.h"
+
+namespace shm_test {
+
+}
---
+++ shm_test/boost_shm_utils.h
@@ -0,0 +1,17 @@
+
+
+/**
+* @file       boost_shm_utils.h
+* @brief      major interfaces: 
+* @author     wangyoucao577@gmail.com
+*
+*/
+
+#ifndef _MY_TINY_TEST_SHM_TEST_BOOST_SHM_UTILS_H_
+#define _MY_TINY_TEST_SHM_TEST_BOOST_SHM_UTILS_H_
+
+namespace shm_test {
+
+}
+
+#endif
