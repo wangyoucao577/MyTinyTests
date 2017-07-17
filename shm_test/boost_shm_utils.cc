65a4123d88f6e205864e83d22a0da63c0fef0fe3
wangyoucao577 <wangyoucao577@gmail.com>
2017-07-17 11:28:26 +0800

    commit related boost interprocess module headers (only headers required)

--- shm_test/boost_shm_utils.cc
+++ shm_test/boost_shm_utils.cc
@@ -3,4 +3,26 @@
 
 namespace shm_test {
 
+	BoostShm::BoostShm(std::string name, int bytes)
+		: ShmBase(bytes),
+		name_(name)
+	{
+
+	}
+
+	BoostShm::~BoostShm() {
+
+	}
+
+	bool BoostShm::Link() {
+
+		return true;
+	}
+	void BoostShm::Unlink() {
+
+	}
+	void BoostShm::UnMap() {
+
+	}
+
 }
--- shm_test/boost_shm_utils.h
+++ shm_test/boost_shm_utils.h
@@ -10,7 +10,25 @@
 #ifndef _MY_TINY_TEST_SHM_TEST_BOOST_SHM_UTILS_H_
 #define _MY_TINY_TEST_SHM_TEST_BOOST_SHM_UTILS_H_
 
+#include "boost/interprocess/managed_shared_memory.hpp"
+
+#include "shm_utils_base.h"
+
 namespace shm_test {
+
+    class BoostShm : public ShmBase {
+	public:
+		BoostShm(std::string name, int bytes);
+		~BoostShm();
+
+	public:
+		bool Link() override;
+		void Unlink() override;
+		void UnMap() override;
+        
+	private:
+		std::string name_;
+    }
 
 }
 
