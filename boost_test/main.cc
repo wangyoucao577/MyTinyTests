0a4a99e628a53a97257c762d02b8e136540da124
wangyoucao577 <wangyoucao577@gmail.com>
2017-08-16 11:33:23 +0800

    test for date_time, need to link libboost_date_time

--- boost_test/main.cc
+++ boost_test/main.cc
@@ -1,7 +1,28 @@
 
+#include <string>
+#include <iostream>
 #include "boost/date_time/posix_time/posix_time.hpp"
 
+using namespace std;
+
+void posix_time_test(){
+
+    boost::posix_time::ptime current_ptime = boost::posix_time::second_clock::local_time();
+    string ptime_iso_str = boost::posix_time::to_iso_string(current_ptime);
+    string ptime_simple_str = boost::posix_time::to_simple_string(current_ptime);
+
+    cout << "iso second clock local time: " << ptime_iso_str << endl;
+    cout << "simple second clock local time: " << ptime_simple_str << endl;
+
+    boost::posix_time::ptime microsec_current_ptime = boost::posix_time::microsec_clock::local_time();
+    cout << "iso microsec clock local time: " << boost::posix_time::to_iso_string(microsec_current_ptime) << endl;
+    cout << "simple microsec clock local time: " << boost::posix_time::to_simple_string(microsec_current_ptime) << endl;
+
+}
+
 int main(){
+
+    posix_time_test();
 
     return 0;
 }
