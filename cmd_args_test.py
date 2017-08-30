2956b38f167ffe1731644940a6ddcbf220565a4d
wangyoucao577 <wangyoucao577@gmail.com>
2017-08-30 23:03:57 +0800

    refactor var names add version optional cmd

--- cmd_args_test.py
+++ cmd_args_test.py
@@ -4,28 +4,29 @@ import argparse
 class MyArgs:
 
     def __init__(self):
-        self.parser = argparse.ArgumentParser(description='test for python cmd argparse module, maybe also optional getopt module.')
+        self.__parser = argparse.ArgumentParser(description='Test for python cmd argparse module, maybe also optional getopt module.')
 
         # default positional arguments
-        self.parser.add_argument('square', help='display a square of a given number', type=int)
+        self.__parser.add_argument('square', help='display a square of a given number', type=int)
 
         # `-*` or `--*` means optional arguments
         #parser.add_argument('--echo', help='echo the string you use enter') # require follow content unless set one by action
         
         # cannot both exist due to `add_mutually_exclusive_group`
-        self.verbose_group = self.parser.add_mutually_exclusive_group()
-        self.verbose_group.add_argument('-v', '--verbose', action='count', default=0, help='display verbose log') 
-        self.verbose_group.add_argument('-q', '--quiet', action='store_true', help='display less log') 
-        #parser.add_argument('-v', '--verbose', action='count', default=0, help='display verbose log') 
+        verbose_group = self.__parser.add_mutually_exclusive_group()
+        verbose_group.add_argument('-v', '--verbose', action='count', default=0, help='display verbose log') 
+        verbose_group.add_argument('-q', '--quiet', action='store_true', help='display less log') 
+        
+        self.__parser.add_argument('-V', '--version', action='version', help='display version info', version='%(prog)s 0.0.1') 
 
         # parse args
-        self.arguments = self.parser.parse_args()
+        self.__args = self.__parser.parse_args()
 
-    def args(self):
-        return self.arguments
+    def get_args(self):
+        return self.__args
 
 def main():
-    args = MyArgs().args()   # construct and parse cmd args  
+    args = MyArgs().get_args()   # construct and parse cmd args  
     
     if args.square:
         answer = args.square ** 2
