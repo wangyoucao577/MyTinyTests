53fef9c13b7237c1fc5127c28c2186d737354188
wangyoucao577 <wangyoucao577@gmail.com>
2017-09-04 22:14:37 +0800

    also support autotools to build

---
+++ cmake_test/autogen.sh
@@ -0,0 +1,15 @@
+
+aclocal
+autoconf
+autoheader
+
+
+touch INSTALL NEWS README AUTHORS ChangeLog COPYING
+touch install-sh missing
+
+automake -i
+
+if [ ! -d _build ]; then 
+    mkdir _build/
+fi
+
