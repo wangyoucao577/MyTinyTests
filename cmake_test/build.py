a08c89f3c063652e44f74d2b23f68ff7d169d707
wangyoucao577 <wangyoucao577@gmail.com>
2017-09-04 23:29:55 +0800

    build.py support both cmake and autotools

--- cmake_test/build.py
+++ cmake_test/build.py
@@ -61,42 +61,8 @@ def dump_version():
     do_cmd("autoconf --version")
     do_cmd("automake --version")
     
+def cmake_build(build_type):
 
-
-class BuildArgs:
-
-    def __init__(self):
-        self.__parser = argparse.ArgumentParser(description='Call cmake/autotools to build codes on indicated platform. Default build with cmake/release.')
-        
-        # version
-        # not use normal version action here, because we hope to dump gcc version here actually
-        self.__parser.add_argument('-v', '--version', action='store_true', help='display cmake/autotools version info')
-        #self.__parser.add_argument('-v', '--version', action='version', help='display version info', version='%(prog)s 0.0.1')
-
-        # clean
-        self.__parser.add_argument('-c', '--clean', action='store_true', help='clean all output from cmake/autotools')
-
-        # build_type
-        self.__parser.add_argument('-b', '--build_type', help='build as debug(-g -DDEBUG) or release(-O2 -UDEBUG), default release', \
-            choices=['debug', 'release'], default='release')
-
-        # parse
-        self.__args = self.__parser.parse_args()
-
-    def get_args(self):
-        return self.__args
-
-def main():
-
-    args = BuildArgs().get_args()
-    if args.version:
-        dump_version()
-        exit(0)
-    if args.clean:
-        cmake_clean()
-        exit(0)
-    build_type = kBuildTypes[args.build_type]   # set cmake build_type for compile
-    
     # prepare build folders 
     do_mkdir(kBuildFolder) 
     os.chdir(kBuildFolder)
@@ -124,6 +90,71 @@ def main():
     do_cmd(cmake_cmd)
     do_cmd(cmake_build_cmd)
     os.chdir("../../")
+
+def autotools_build(build_type):
+
+    # call autogen.sh
+    if os.path.isfile(kAutogen):
+        do_cmd(kAutogen)
+    else:
+        print "no valid " + kAutogen + " file."
+        return False
+
+    # prepare build folders 
+    do_mkdir(kBuildFolder) 
+    os.chdir(kBuildFolder)
+    sub_dir_name = build_type
+    do_mkdir(sub_dir_name)
+    os.chdir(sub_dir_name)
+
+    # configure and build
+    do_cmd('../../configure')
+    do_cmd('make -j')
+    os.chdir("../../")
+
+class BuildArgs:
+
+    def __init__(self):
+        self.__parser = argparse.ArgumentParser(description='Call cmake/autotools to build codes on indicated platform. Default build with cmake/release.')
+        
+        # version
+        # not use normal version action here, because we hope to dump gcc version here actually
+        self.__parser.add_argument('-v', '--version', action='store_true', help='display cmake/autotools version info')
+        #self.__parser.add_argument('-v', '--version', action='version', help='display version info', version='%(prog)s 0.0.1')
+
+        # clean
+        self.__parser.add_argument('-c', '--clean', action='store_true', help='clean all output from cmake/autotools')
+
+        # build_system
+        self.__parser.add_argument('-B', '--build_system', help='build by cmake or GNU autotools, default cmake', \
+            choices=['cmake', 'autotools'], default='cmake')
+
+        # build_type
+        self.__parser.add_argument('-b', '--build_type', help='build as debug(-g -DDEBUG) or release(-O2 -UDEBUG), default release', \
+            choices=['debug', 'release'], default='release')
+
+        # parse
+        self.__args = self.__parser.parse_args()
+
+    def get_args(self):
+        return self.__args
+
+def main():
+
+    args = BuildArgs().get_args()
+    if args.version:
+        dump_version()
+        exit(0)
+    if args.clean:
+        cmake_clean()
+        exit(0)
+    build_type = kBuildTypes[args.build_type]   # set cmake build_type for compile
+    
+    if args.build_system == 'autotools':
+        autotools_build(build_type)
+    else:
+        cmake_build(build_type)
+
 
 if __name__ == '__main__':
     main()
