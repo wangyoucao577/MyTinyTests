904882d7385be4f12f5c06e1ff2f1e7a94de3477
wangyoucao577 <wangyoucao577@gmail.com>
2017-09-28 13:16:15 +0800

    determin current binary 32bit or 64bit by sizeof(void*)

---
+++ c_cc_misc/determin_binary_32_64bit.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	printf("sizeof(int) %d, sizeof(long int) %d, sizeof(void*) %d\n", sizeof(int), sizeof(long int), sizeof(void*));
+
+    const int k64bitSize = 8 * sizeof(char);
+    const int k32bitSize = 4 * sizeof(char);
+
+    if (sizeof(void*) == k64bitSize){
+        printf("This is a 64-bit binary.\n");    
+    }else if (sizeof(void*) == k32bitSize){
+        printf("This is a 32-bit binary.\n");    
+    }else{
+        printf("Unknown bit of this binary.\n");
+    }
+
+	return 0;
+}
