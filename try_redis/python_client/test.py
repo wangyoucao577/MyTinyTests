e7f4903a434fe0af6a25ed66747dd9c3674f364b
wangyoucao577 <wangyoucao577@gmail.com>
2018-06-21 09:05:05 -0700

    test rejson by redis-py

--- try_redis/python_client/test.py
+++ try_redis/python_client/test.py
@@ -50,11 +50,30 @@ def test_basic_commands(r):
     print reply
 
     print "exit " + test_basic_commands.__name__ 
-    
+
+def test_rejson(r):
+    print "enter " + test_rejson.__name__ 
+
+    json_str = "{\"key1\":\"value\", \"array\":[{\"key2\":\"value2\"}, {\"key2\":\"value3\"}, {\"key2\":\"value4\"}]}";
+
+    reply = r.execute_command('JSON.SET', 'json2', '.', json_str)
+    print reply
+
+    reply = r.execute_command('JSON.GET', 'json2', '.')
+    print reply
+
+    reply = r.execute_command('JSON.GET', 'json2', '.key1')
+    print reply
+
+    reply = r.execute_command('JSON.GET', 'json2', '.array')
+    print reply
+
+    print "exit  " + test_rejson.__name__ 
+
 def main():
     r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
     test_basic_commands(r)
-
+    test_rejson(r)
 
 
 main()
