b7a35505797c87785b19412923c016d499f28e78
wangyoucao577 <wangyoucao577@gmail.com>
2018-07-03 01:19:24 -0700

    simplest test code for memcached

--- try_redis/memcached_cc_client/main.cc
+++ try_redis/memcached_cc_client/main.cc
@@ -1,8 +1,36 @@
 
 
+#include <stdio.h>
+#include <assert.h>
+
 #include <libmemcached/memcached.h>
 
 
 int main(){
+    const char *config_string = "--SERVER=localhost:11211";
+    memcached_st *memc = memcached(config_string, strlen(config_string));
+    
+    const char *key= "foo";
+    const char *value= "value";
+    memcached_return_t rc= memcached_set(memc, key, strlen(key), value, strlen(value), (time_t)0, (uint32_t)0);
+    if (rc != MEMCACHED_SUCCESS) {
+        printf("memcached_set failed, error %d\n", rc);
+    } else {
+        printf("memcached_set %s succeed\n", key);
+    }
+
+    size_t value_length = 0;
+    uint32_t flags = 0;
+    memcached_return_t err;
+    char* got_value = memcached_get(memc, key, strlen(key), &value_length, &flags, &err);
+    if (err != MEMCACHED_SUCCESS) {
+        printf("memcached_get failed, error %d\n", err);
+    } else {
+        assert(NULL != got_value);
+        printf("VALUE %s %s %u %ld\n", key, got_value, flags, value_length);
+    }
+    
+    memcached_free(memc);
+
     return 0;
 }
