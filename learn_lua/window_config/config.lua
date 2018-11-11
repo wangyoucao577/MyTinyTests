20b481feab35270c34de18ef9991b43527bb504a
wangyoucao577 <wangyoucao577@gmail.com>
2018-11-11 16:13:39 +0800

    one more config: background color

--- learn_lua/window_config/config.lua
+++ learn_lua/window_config/config.lua
@@ -7,3 +7,9 @@ if os.getenv("DISPLAY") == "LARGER" then
     width = 300
     height = 300
 end
+
+-- 背景颜色
+BlueRGB = {red = 0, green = 0, blue = 1.0}
+RedRGB = {red = 1.0, green = 0, blue = 0}
+
+background = BlueRGB
--- learn_lua/window_config/main.c
+++ learn_lua/window_config/main.c
@@ -21,9 +21,28 @@ int getglobalint(lua_State *L, const char* var_name) {
     return result;
 }
 
+// 调用时table应位于栈顶
+double getcolorfield(lua_State *L, const char* key) {
+    lua_pushstring(L, key);
+    lua_gettable(L, -2);    // get t[key]
+
+    if (!lua_isnumber(L, -1)) {
+        luaL_error(L, "value of key %s is not a float number in table", key);
+    }
+    double result = lua_tonumber(L, -1);
+    lua_pop(L, 1);
+    return result;
+}
+
 struct config_t {
     int width;
     int height;
+
+    struct {
+        double red;
+        double green;
+        double blue;
+    } background_color;
 };
 
 int load_config(struct config_t* cfg, const char* file_path) {
@@ -40,8 +59,30 @@ int load_config(struct config_t* cfg, const char* file_path) {
     cfg->width = getglobalint(L, "width");
     cfg->height = getglobalint(L, "height");
 
+    lua_getglobal(L, "background");
+    if (!lua_istable(L, -1)){
+        luaL_error(L, "'background' is not a table");
+    }
+    cfg->background_color.red = getcolorfield(L, "red");
+    cfg->background_color.green = getcolorfield(L, "green");
+    cfg->background_color.blue = getcolorfield(L, "blue");
+
+
     lua_close(L);
     return 0;
+}
+
+void dump_config(const struct config_t* cfg) {
+    if (cfg == NULL) {
+        return;
+    }
+
+    printf("loaded config: \n");
+    printf("    width = %d\n", cfg->width);
+    printf("    height = %d\n", cfg->height);
+    printf("    background_color.red = %g\n", cfg->background_color.red);
+    printf("    background_color.green = %g\n", cfg->background_color.green);
+    printf("    background_color.blue = %g\n", cfg->background_color.blue);
 }
 
 int main(int argc, char* argv[]) {
@@ -50,10 +91,10 @@ int main(int argc, char* argv[]) {
     if (argc >= 2) {
         file_path = argv[1];
     }
-    
+
     struct config_t cfg;
     load_config(&cfg, file_path);
-    printf("loaded width %d height %d\n", cfg.width, cfg.height);
+    dump_config(&cfg);
 
     return 0;
 }
