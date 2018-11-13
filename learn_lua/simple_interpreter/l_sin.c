aaab6c95f245f1c17473c04e182913750b9d0d09
wangyoucao577 <wangyoucao577@gmail.com>
2018-11-13 22:37:03 +0800

    error handle for mysin

--- learn_lua/simple_interpreter/l_sin.c
+++ learn_lua/simple_interpreter/l_sin.c
@@ -4,7 +4,7 @@
 #include "l_sin.h"
 
 int l_sin(lua_State *L){
-    double d = lua_tonumber(L, 1); // 从栈中获取参数
+    double d = luaL_checknumber(L, 1); // 从栈中获取参数
     lua_pushnumber(L, sin(d));  // 返回值压入栈
     return 1;
 }
