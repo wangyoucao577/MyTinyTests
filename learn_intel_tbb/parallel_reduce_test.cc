1b2ec0524323c8d0b7f6c5176136d8191d6563dc
wangyoucao577 <wangyoucao577@gmail.com>
2018-12-06 22:52:01 +0800

    find min value by parallel_reduce - the most important thing is whether the tasks can be reduce

--- learn_intel_tbb/parallel_reduce_test.cc
+++ learn_intel_tbb/parallel_reduce_test.cc
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <float.h>
 
 #include "tbb/tbb.h"
 
@@ -43,6 +44,44 @@ float ParallelSumFoo(float a[], size_t n) {
     return sf.my_sum;
 }
 
+class MinFoo {
+    const float *const my_a;
+public:
+    float value_of_min;
+    long index_of_min;
+
+    void operator() (const tbb::blocked_range<size_t>& r) {
+        const float* a = my_a;
+        for (size_t i = r.begin(); i != r.end(); ++i){
+            float value = foo(a[i]);
+            if (value < value_of_min) {
+                value_of_min = value;
+                index_of_min = i;
+            }
+        }
+    }
+
+    MinFoo(MinFoo& x, tbb::split) : 
+        my_a(x.my_a), value_of_min(FLT_MAX), index_of_min(-1) {}
+
+    void join(const MinFoo& y) {
+        if (y.value_of_min < value_of_min) {
+            value_of_min = y.value_of_min;
+            index_of_min = y.index_of_min;
+        }
+    }
+
+    MinFoo(const float a[]) : 
+        my_a(a), value_of_min(FLT_MAX), index_of_min(-1) {}
+};
+
+float ParallelFindMinFoo(float a[], size_t n) {
+    MinFoo mf(a);
+    parallel_reduce(tbb::blocked_range<size_t>(0, n), mf);
+    return mf.value_of_min;
+}
+
+
 void test_functional() {
 
     std::cout << "Enter " << __FUNCTION__ << std::endl;
@@ -69,7 +108,7 @@ void test_functional() {
     std::cout << "Exit " << __FUNCTION__ << std::endl << std::endl;
 }
 
-void test_parallel() {
+void test_parallel_sum() {
 
     std::cout << "Enter " << __FUNCTION__ << std::endl;
 
@@ -100,10 +139,42 @@ void test_parallel() {
     std::cout << "Exit " << __FUNCTION__ << std::endl << std::endl;
 }
 
+void test_parallel_find_min() {
+
+    std::cout << "Enter " << __FUNCTION__ << std::endl;
+
+    const int n = 1000000000;
+    float* a = (float*)malloc(n * sizeof(float));
+    if (!a){
+        std::cerr << "malloc failed, size " << n << std::endl;
+        return;
+    }
+
+    // initialize values
+    std::cout << "parallel initialize..." << std::endl;
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, n), [a](const tbb::blocked_range<size_t>& r){
+        for (size_t i = r.begin(); i != r.end(); ++i) {
+            a[i] = 1.0f;
+        }
+    });
+
+    std::cout << "parallel reduce..." << std::endl;
+    float p_sum = ParallelFindMinFoo(a, n);
+
+    std::cout << "parallel done..." << std::endl;
+
+    std::cout << "min by parallel: " << p_sum << std::endl;
+
+    free(a);
+
+    std::cout << "Exit " << __FUNCTION__ << std::endl << std::endl;
+}
+
 
 int main(int argc, char* argv[]) {
 
     test_functional();
-    test_parallel();
+    test_parallel_sum();
+    test_parallel_find_min();
     return 0;
 }
