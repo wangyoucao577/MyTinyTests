59201a588341846d1b8b9fed70616afded517fca
wangyoucao577 <wangyoucao577@gmail.com>
2018-12-15 22:30:16 +0800

    test for access value by shared-ptr; fix make_shared wrong usage

--- test_shared_ptr/src/loop_pass_pointers.hpp
+++ test_shared_ptr/src/loop_pass_pointers.hpp
@@ -4,7 +4,7 @@
 
 template<typename AcceptT, typename SampleT>
 void loop_pass_pointers(){
-    my_shared_ptr<SampleT> ptr = MY_NAMESPACE::make_shared<SampleT>(SampleT());
+    my_shared_ptr<SampleT> ptr = MY_NAMESPACE::make_shared<SampleT>();
 
     constexpr int64_t kLoopCount = 10000000;
     for (int i = 0; i < kLoopCount; ++i) {
