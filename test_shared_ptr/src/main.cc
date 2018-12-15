8bb697451836f1ea5ba9e2717893fb610a911c79
wangyoucao577 <wangyoucao577@gmail.com>
2018-12-15 21:40:37 +0800

    refactor mock classes to template too

--- test_shared_ptr/src/main.cc
+++ test_shared_ptr/src/main.cc
@@ -6,24 +6,26 @@
 #include "mock_classes.h"
 
 
-template<typename T>
+template<typename AcceptT, typename SampleT>
 void loop_pass_pointers(){
-    my_shared_ptr<SampleObject> ptr = MY_NAMESPACE::make_shared<SampleObject>(SampleObject());
+    my_shared_ptr<SampleT> ptr = MY_NAMESPACE::make_shared<SampleT>(SampleT());
 
     for (int i = 0; i < 10000000; ++i) {
-        auto&& t = T(ptr);
+        auto&& t = AcceptT(ptr);
     }
 }
+
+class SampleObject{};
 
 int main() {
  
  
     auto start = std::chrono::system_clock::now();
-    loop_pass_pointers<AcceptSharedPtr>();
+    loop_pass_pointers<AcceptSharedPtr<SampleObject>, SampleObject>();
     auto time_point_1 = std::chrono::system_clock::now();
-    loop_pass_pointers<AcceptConstReferenceSharedPtr>();
+    loop_pass_pointers<AcceptConstReferenceSharedPtr<SampleObject>, SampleObject>();
     auto time_point_2 = std::chrono::system_clock::now();
-    loop_pass_pointers<AcceptConstReferenceSharedPtrWithConstObject>();
+    loop_pass_pointers<AcceptConstReferenceSharedPtrWithConstObject<SampleObject>, SampleObject>();
     auto end = std::chrono::system_clock::now();
 
     std::chrono::duration<double> elapsed_seconds_1 = time_point_1 - start;
--- test_shared_ptr/src/mock_classes.h
+++ test_shared_ptr/src/mock_classes.h
@@ -1,32 +1,34 @@
 
 #include "my_shared_ptr.hpp"
 
-class SampleObject{
-};
 
+
+template<typename T>
 class AcceptSharedPtr{
 public:
-    AcceptSharedPtr(my_shared_ptr<SampleObject> sample_object)
+    AcceptSharedPtr(my_shared_ptr<T> sample_object)
     : sample_object_(sample_object){}
 
 private:
-    my_shared_ptr<SampleObject> sample_object_;
+    my_shared_ptr<T> sample_object_;
 };
 
+template<typename T>
 class AcceptConstReferenceSharedPtr{
 public:
-    AcceptConstReferenceSharedPtr(const my_shared_ptr<SampleObject> & sample_object)
+    AcceptConstReferenceSharedPtr(const my_shared_ptr<T> & sample_object)
     : sample_object_(sample_object){}
 
 private:
-    my_shared_ptr<SampleObject> sample_object_;
+    my_shared_ptr<T> sample_object_;
 };
 
+template<typename T>
 class AcceptConstReferenceSharedPtrWithConstObject{
 public:
-    AcceptConstReferenceSharedPtrWithConstObject(const my_shared_ptr<const SampleObject> & sample_object)
+    AcceptConstReferenceSharedPtrWithConstObject(const my_shared_ptr<const T> & sample_object)
     : sample_object_(sample_object){}
 
 private:
-    my_shared_ptr<const SampleObject> sample_object_;
+    my_shared_ptr<const T> sample_object_;
 };
